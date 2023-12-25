33de1a831d1b5675b22d7c04b630206706a62cd7
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2023-12-25 10:06:54 +0400

    fix: rename compute workflows ids. delete compute percent_expenses from run_script

--- ozon_task/run_script.sh
+++ ozon_task/run_script.sh
@@ -6,4 +6,3 @@ python /home/sammy/tasks/temporalio/ozon_task/workflow_products.py
 python /home/sammy/tasks/temporalio/ozon_task/workflow_stocks.py
 python /home/sammy/tasks/temporalio/ozon_task/workflow_transactions.py
 python /home/sammy/tasks/temporalio/ozon_task/workflow_compute_coefs_and_groups.py
-python /home/sammy/tasks/temporalio/ozon_task/workflow_percent_expenses.py
