465fca5d7e9083f2377dd9087c069bf92751af3c
mechanovkirill <mechanovkirill@gmail.com>
2024-03-04 15:04:35 +0400

    Create workflow to import transactions data for period.

---
+++ ozon/fill_db_workflow_transactions_for_period.py
@@ -0,0 +1,76 @@
+import asyncio
+from datetime import timedelta
+
+from temporalio import workflow, activity
+from temporalio.client import Client
+from temporalio.common import RetryPolicy
+from temporalio.worker import Worker
+
+
+with workflow.unsafe.imports_passed_through():
+    from activities import (
+        activity_import_transactions_from_prev_2_years,
+        activity_write_transactions_to_odoo,
+        activity_remove_csv_files,
+    )
+    from tools import ImportLogging
+
+@activity.defn
+async def activity_create_mass_data_import() -> None:
+    await ImportLogging().create_mass_data_import({'name': 'Импорт транзакций за период', 'logged_activities_qty': 1})
+
+
+@workflow.defn
+class OzonPostingsForPeriodWorkflow:
+    @workflow.run
+    async def run(self) -> None:
+        await workflow.execute_activity(
+            activity_create_mass_data_import,
+            start_to_close_timeout=timedelta(minutes=5),
+            retry_policy=RetryPolicy(maximum_interval=timedelta(minutes=2)),
+        )
+
+        await workflow.execute_activity(
+            activity_import_transactions_from_prev_2_years,
+            start_to_close_timeout=timedelta(seconds=20000),
+            retry_policy=RetryPolicy(maximum_interval=timedelta(hours=24)),
+        )
+
+        await workflow.execute_activity(
+            activity_write_transactions_to_odoo,
+            start_to_close_timeout=timedelta(seconds=20000),
+            retry_policy=RetryPolicy(maximum_interval=timedelta(hours=24)),
+        )
+
+        await workflow.execute_activity(
+            activity_remove_csv_files,
+            start_to_close_timeout=timedelta(seconds=20000),
+            retry_policy=RetryPolicy(maximum_interval=timedelta(hours=24)),
+        )
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+
+    async with Worker(
+        client,
+        task_queue="ozon-task-queue",
+        workflows=[OzonPostingsForPeriodWorkflow],
+        activities=[
+            activity_create_mass_data_import,
+            activity_import_transactions_from_prev_2_years,
+            activity_write_transactions_to_odoo,
+            activity_remove_csv_files,
+        ],
+    ):
+        handle = await client.start_workflow(
+            OzonPostingsForPeriodWorkflow.run,
+            id="ozon-workflow-transactions-prev-month-id",
+            task_queue="ozon-task-queue",
+        )
+
+        await handle.result()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
